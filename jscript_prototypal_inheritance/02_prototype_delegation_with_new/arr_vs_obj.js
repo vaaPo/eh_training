be6dadc8eaf3234925423ebff09599ceae977b8c
vaaPo <ripraptiptop@gmail.com>
2018-11-08 15:22:45 +0200

    wheels 5,4,1

---
+++ jscript_prototypal_inheritance/02_prototype_delegation_with_new/arr_vs_obj.js
@@ -0,0 +1,9 @@
+console.log({})
+console.log({})
+
+const obj=new Object()
+const arr=new Array()
+
+console.log({obj})
+console.log({arr})
+
--- jscript_prototypal_inheritance/02_prototype_delegation_with_new/car.js
+++ jscript_prototypal_inheritance/02_prototype_delegation_with_new/car.js
@@ -2,9 +2,13 @@ function Car(make) {
     this.make = make
     this.wheels = 1
 }
+Car.wheels=5
 Car.prototype.color='black'
 Car.prototype.wheels=4
 
 console.log(Car.prototype)
 const myCar =new Car('Ford')
 console.log(myCar.color)
+console.log(myCar.wheels)
+console.log(Car.wheels)
+console.log(Car.prototype.wheels)
