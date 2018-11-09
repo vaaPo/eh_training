a8e21503bee1c35a6886a7a81e8dc58277670642
vaaPo <ripraptiptop@gmail.com>
2018-11-09 14:23:14 +0200

    see how 'use strict' affects and how writable works, compare files

---
+++ jscript_prototypal_inheritance/12_polymorphism/writable_foobar.js
@@ -0,0 +1,44 @@
+const foo={
+    name:'tyler'
+}
+const bar={
+    lastname:'clark'
+}
+Object.setPrototypeOf(bar,foo)
+console.log(bar)
+console.log(foo)
+console.log(foo.name)
+console.log(foo.lastname)
+console.log(bar.name)
+console.log(bar.lastname)
+
+bar.name='james'
+console.log(bar)
+console.log(foo)
+console.log(foo.name)
+console.log(foo.lastname)
+console.log(bar.name)
+console.log(bar.lastname)
+
+const xyz={
+    //
+}
+Object.defineProperty(xyz,'name',{
+    value: 'tyler',
+    writable: false
+})
+const zy={
+    lastname:'clark'
+}
+Object.setPrototypeOf(zy,xyz)
+zy.name='paula' //doesnt change due writable is false
+console.log(zy)
+console.log(xyz)
+console.log(xyz.name)
+console.log(xyz.lastname)
+console.log(zy.name) // this is tyler because writable is false
+console.log(zy.lastname)
+
+
+
+
