5cb1ed4bc8889e476c091811fc2277fe88a754f0
vaaPo <ripraptiptop@gmail.com>
2018-11-10 20:19:09 +0200

    walk the talk

---
+++ npx/egghead-npx/lessons/01-use-npx-to-run-locally-installed-node-modules/vaapo-npx-test/terminal-commands.sh
@@ -0,0 +1,12 @@
+ 2005  npm init -y
+ 2006  npm i eslint -D
+ 2007  npm ls eslint
+ 2008  eslint --init
+ 2009  ./node_modules/.bin/eslint --init
+ 2010  $(npm bin)/eslint --init
+ 2011  npx eslint --init
+ 2012  npx eslint --cache --fix ./
+ 2013  npm run
+ 2014  npm run lint
+ 2015  npm run lint -s
+ 
