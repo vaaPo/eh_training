9dab53def7c5c6c039dae24fe9691eb07749d65f
vaaPo <ripraptiptop@gmail.com>
2018-11-12 18:12:10 +0200

    couple comments to clarify

--- start_react/start-learning-react/lessons/05-react-state-basics/App.js
+++ start_react/start-learning-react/lessons/05-react-state-basics/App.js
@@ -4,21 +4,22 @@ import React from 'react';
 
 class App extends React.Component {
   constructor(){
-    super();
+    super();    //This is going to give the keyword this the context within our component, rather than its parent class React.Component.
     this.state = {
-      txt: 'this is the state txt',
+      txt: 'this is the state txt',       // txt is key
       cat: 0
     }
   }
   update(e){
-    this.setState({txt: e.target.value})
+    this.setState({txt: e.target.value})    //It's important to note here that, the call of this.setState only overwrites the values that we actually send into it.
   }
+  // interpolating {this.state.txt} in JSX
   render(){
     return (
       <div>
         <input type="text"
           onChange={this.update.bind(this)} />
-          <h1>{this.state.txt} - {this.state.cat}</h1>
+          <h1>{this.state.txt} - {this.state.cat}</h1>        
       </div>
     )
   }
