2d153f98495dc73b2161b03fc0300b666faa79d5
vaaPo <ripraptiptop@gmail.com>
2018-11-12 20:08:05 +0200

    some comments again

--- start_react/start-learning-react/lessons/14-react-dynamically-generated-components/App.js
+++ start_react/start-learning-react/lessons/14-react-dynamically-generated-components/App.js
@@ -4,11 +4,11 @@ import React from 'react';
 
 class App extends React.Component {
   constructor(){
-    super();
+    super();                        // call super() to get our context
     this.state = {items: []}
   }
   componentWillMount(){
-    fetch( 'https://swapi.co/api/people/?format=json' )
+    fetch( 'https://swapi.co/api/people/?format=json' )   //use fetch to make an AJAX call to the Star Wars API. We'll just hit up the people endpoint there.
       .then( response => response.json() )
       .then( ({results: items}) => this.setState({items}))
   }
@@ -16,7 +16,7 @@ class App extends React.Component {
     this.setState({filter: e.target.value})
   }
   render(){
-    let items = this.state.items
+    let items = this.state.items                          //That's going to set that data up.
     if(this.state.filter){
       items = items.filter( item =>
       item.name.toLowerCase()
@@ -26,13 +26,14 @@ class App extends React.Component {
       <div>
         <input type="text"
           onChange={this.filter.bind(this)}/>
-        {items.map(item =>
-          <Person key={item.name} person={item} />)}
+        {items.map(item =>                                //interpolate items.map - see row 8 - its and array
+                                                          //item.name (due no id) as key to avoid "each child in an array or iterator should have a unique key prop."
+          <Person key={item.name} person={item} />)}     
       </div>
     )
   }
 }
 
-const Person = (props) => <h4>{props.person.name}</h4>
+const Person = (props) => <h4>{props.person.name}</h4>    // capital P ! This is Person component with h4, summoned in the above return
 
 export default App
