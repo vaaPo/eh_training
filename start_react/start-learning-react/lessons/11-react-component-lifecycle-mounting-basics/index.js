9d04e01a3b66b8551ddd60d8665f321a3157128b
vaaPo <ripraptiptop@gmail.com>
2018-11-12 19:29:17 +0200

    Chnaged to render Wrapper from ./App to make more sense

--- start_react/start-learning-react/lessons/11-react-component-lifecycle-mounting-basics/index.js
+++ start_react/start-learning-react/lessons/11-react-component-lifecycle-mounting-basics/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('app'));
+import Wrapper from './App';
+
+//why it is not Wrapper to be rendered?
+//ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Wrapper />, document.getElementById('app'));
