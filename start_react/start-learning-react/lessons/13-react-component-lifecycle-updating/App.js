6ec40e86b6af8a394c95209b90c22ad9c1364de8
vaaPo <ripraptiptop@gmail.com>
2018-11-12 19:46:27 +0200

    some comments

--- start_react/start-learning-react/lessons/13-react-component-lifecycle-updating/App.js
+++ start_react/start-learning-react/lessons/13-react-component-lifecycle-updating/App.js
@@ -5,19 +5,19 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(){
-    super();
-    this.state = {increasing: false}
+    super();                                // so this can be used
+    this.state = {increasing: false}        // set state to false for "increasing"
   }
   update(){
     ReactDOM.render(
-      <App val={this.props.val + 1} />,
-      document.getElementById('root'))
+      <App val={this.props.val + 1} />,     // render with val+1
+      document.getElementById('root'))      //We're actually going to rerender our entire component.
   }
   componentWillReceiveProps(nextProps){
-    this.setState({increasing: nextProps.val > this.props.val})
+    this.setState({increasing: nextProps.val > this.props.val})   // will print false for first 0 and after that true always because increasing 1,2,3,4,...
   }
   shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.val % 5 === 0;
+    return nextProps.val % 5 === 0;                    // will printe when 5,10,15,20...
   }
   render(){
     console.log(this.state.increasing)
@@ -28,7 +28,7 @@ class App extends React.Component {
     )
   }
   componentDidUpdate(prevProps, prevState) {
-    console.log(`prevProps: ${prevProps.val}`)
+    console.log(`prevProps: ${prevProps.val}`)          // will print 4 for 5th click, 9 for 10th click, 14 for 15th click..
   }
 }
 
