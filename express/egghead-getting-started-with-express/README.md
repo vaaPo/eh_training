f6ee819c1848d5e93d27d54045c32aa2bbfade1a
vaaPo <ripraptiptop@gmail.com>
2018-11-14 11:32:47 +0200

    init, this is just for vaaPo - use original instead

---
+++ express/egghead-getting-started-with-express/README.md
@@ -0,0 +1,11 @@
+# vaaPo tampered lesson files from eh
+
+# Getting Started with Express.js
+
+Code examples from the [Getting Started with Express.js](https://egghead.io/series/getting-started-with-express-js) series on [https://egghead.io](https://egghead.io)
+
+## Importing data
+
+You can populate your database with the following command.
+
+`mongoimport --db test --collection users --drop --file user_list.json`
---
+++ express/egghead-getting-started-with-express/username.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var helpers = require('./helpers');
+var fs = require('fs');
+
+var User = require('./db').User;
+
+var router = express.Router({
+  mergeParams: true
+});
+
+router.use(function (req, res, next) {
+  console.log(req.method, 'for', req.params.username, 'at', req.path);
+  next();
+});
+
+router.get('/', function (req, res) {
+  var username = req.params.username;
+  User.findOne({username: username}, function (err, user) {
+    if (err) console.error(err);
+    res.render('user', {
+      user: user,
+      address: user.location
+    });
+  });
+});
+
+router.use(function (err, req, res, next) {
+  if (err) console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+router.put('/', function (req, res) {
+  var username = req.params.username;
+
+  User.findOne({username: username}, function (err, user) {
+    if (err) console.error(err);
+
+    user.name.full = req.body.name;
+    user.location = req.body.location;
+    user.save(function () {
+      res.end();
+    });
+  });
+});
+
+router.delete('/', function (req, res) {
+  var fp = helpers.getUserFilePath(req.params.username);
+  fs.unlinkSync(fp); // delete the file
+  res.sendStatus(200);
+});
+
+module.exports = router;
